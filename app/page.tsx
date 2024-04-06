415c9d8774ab92723137ef08374a019db0e018c8
magmafellow <magmafellow@gmail.com>
2024-04-06 21:34:27 +0300

    /shop moved to /

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,60 @@
+import Link from "next/link";
+import Thing from "@/app/ui/thing";
+import styles from "@/app/page.module.css";
+import { dosis } from "@/app/ui/fonts";
+import { sneakers } from "@/app/lib/placeholder";
+
 export default function Page() {
-  return 1
+  return (
+    <div className={`container ${dosis.className}`}>
+      {/* tags */}
+      <ul className={`${styles.tagContainer}`}>
+        <li>
+          <Link className={`${styles.tagLink}`} href="#">
+            sneakers
+          </Link>
+        </li>
+        <li>
+          <Link className={`${styles.tagLink}`} href="#">
+            T-shirts
+          </Link>
+        </li>
+        <li>
+          <Link className={`${styles.tagLink}`} href="#">
+            caps
+          </Link>
+        </li>
+      </ul>
+      {/* shop things */}
+      <ul className={`${styles.thingGrid}`}>
+        {sneakers.map((sneaker) => (
+          <li key={sneaker.name}>
+            <Thing
+              name={sneaker.name}
+              urlFragment={sneaker.urlFragment}
+              desc={sneaker.desc}
+            />
+          </li>
+        ))}
+        {sneakers.map((sneaker) => (
+          <li key={sneaker.name}>
+            <Thing
+              name={sneaker.name}
+              urlFragment={sneaker.urlFragment}
+              desc={sneaker.desc}
+            />
+          </li>
+        ))}
+        {sneakers.map((sneaker) => (
+          <li key={sneaker.name}>
+            <Thing
+              name={sneaker.name}
+              urlFragment={sneaker.urlFragment}
+              desc={sneaker.desc}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
